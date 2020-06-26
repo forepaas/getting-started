96da9d2d497010450d7788ef57a71c321ecc03a0
Hamza Kadiri <hkadiri@forepaas.com>
2020-06-26 13:38:58 +0200

    Updated zip script to work without argument

--- scripts/utils/zip.sh
+++ scripts/utils/zip.sh
@@ -4,8 +4,13 @@
 # To use the script, simply run `./zip.sh myzip` where myzip is the name of the generated zip file.
 # The zip file is created at the same location as the folder containing this file.
 # You can also put a sencond argument to specify the folder to zip.
-if [[ -z $2 ]]; then
-   zip -r ../$1 . -x "./node_modules/*" -x "*.git*" -x "./forepaas/*"
+dirname=${PWD##*/}
+if [[ -z $1 ]]; then
+   zip -r ../$dirname . -x "./node_modules/*" -x "*.git*" -x "./forepaas/*"
 else
-   cd $2 && zip -r ../$1 . -x "./node_modules/*" -x "*.git*" -x "./forepaas/*"
+   if [[ -z $2 ]]; then
+      zip -r ../$1 . -x "./node_modules/*" -x "*.git*" -x "./forepaas/*"
+   else
+      cd $2 && zip -r ../$1 . -x "./node_modules/*" -x "*.git*" -x "./forepaas/*"
+   fi
 fi
