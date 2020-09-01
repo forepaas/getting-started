8bff8bdebad5580d68503ddf6e074f4be7494eb5
Hamza Kadiri <hkadiri@forepaas.com>
2020-09-01 16:47:33 +0200

    Reset app architecture as before cra

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,16 @@ const CracoAlias = require("craco-alias");
 const CracoLessPlugin = require("craco-less");
 const CracoExtendScope = require("@dvhb/craco-extend-scope");
 
+const path = require("path");
+const fs = require("fs");
+
+const rewireBabelLoader = require("craco-babel-loader");
+
+// helpers
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
 module.exports = {
   babel: {
     plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]]
@@ -14,6 +24,17 @@ module.exports = {
         baseUrl: "."
       }
     },
-    { plugin: CracoLessPlugin }
+    { plugin: CracoLessPlugin },
+    {
+      plugin: rewireBabelLoader,
+      options: {
+        includes: [resolveApp("forepaas")], //put things you want to include in array here
+        excludes: [/(node_modules|bower_components)/] //things you want to exclude here
+        //you can omit include or exclude if you only want to use one option
+      }
+    },
+    { plugin: CracoExtendScope, options: { path: "config" } },
+    { plugin: CracoExtendScope, options: { path: "forepaas" } },
+    { plugin: CracoExtendScope, options: { path: "forepaas.json" } }
   ]
 };
